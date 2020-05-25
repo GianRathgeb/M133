6c8755a96b3d0f98ff7071b1da87ed1af9c74770
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2020-05-25 15:08:12 +0200

    zusatzaufgabe geschaft

--- Aufgabenblatt3/7-2-Zusatz/login.php
+++ Aufgabenblatt3/7-2-Zusatz/login.php
@@ -1,26 +1,26 @@
 <?php
 $filename = "hashes.txt";
-$lines = [];
 $file = fopen($filename, "r");
+$lines = [];
 while ($line = fgets($file)) {
-    $line = explode(";", $line);
     array_push($lines, $line);
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if (in_array($username, $lines)) {
-        foreach ($lines as $login) {
-            if (($login[0] == $username) and (password_verify($password, $login[1]))) {
+    foreach ($lines as $login) {
+        $login = explode(";", $login);
+        if ($login[0] == $username) {
+            if (!password_verify($password, $login[1])) {
                 echo "Login erfolgreich";
-            break;
+                break;
             }
         }
-        echo "Username and/or Password incorrect.";
-    } else {
-        echo "Username and/or Password incorrect.";
     }
+} else {
+    echo "Username and/or Password incorrect.";
 }
+
 
 
 ?>
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>7-2-Login</title>
+    <title>7-2-Zusatz-Login</title>
 </head>
 
 <body>
