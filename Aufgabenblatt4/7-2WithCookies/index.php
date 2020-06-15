8a90af0722d4ec924bf705ba6221ad6fb7a65c1f
GianRathgeb <gian.rathgbe@bbz-sh.ch>
2020-06-15 11:48:57 +0200

    a

--- Aufgabenblatt4/7-2WithCookies/index.php
+++ Aufgabenblatt4/7-2WithCookies/index.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>7-2 Cookies Index.php</title>
 </head>
- 
 <body>
  
     <form action="login.php" method="post">
@@ -16,7 +15,7 @@
         <input type="submit" name="login" value="Login">
  
     </form>
- 
+
 </body>
  
 </html>
